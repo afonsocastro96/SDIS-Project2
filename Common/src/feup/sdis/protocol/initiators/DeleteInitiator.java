98452779d560df472d7c3e0903c7c10fa5d2d6f3
João Silva <joaosilva@revtut.net>
2016-05-14 14:08:16 +0100

    Added runnables.

--- Common/src/feup/sdis/protocol/initiators/DeleteInitiator.java
+++ Common/src/feup/sdis/protocol/initiators/DeleteInitiator.java
@@ -7,9 +7,14 @@ import java.util.Observable;
 /**
  * Created by joaos on 14/05/2016.
  */
-public class DeleteInitiator extends BackupProtocol {
+public class DeleteInitiator extends BackupProtocol implements Runnable {
     @Override
     public void update(Observable o, Object arg) {
+
+    }
+
+    @Override
+    public void run() {
 
     }
 }
--- Common/src/feup/sdis/protocol/listeners/GetChunkListener.java
+++ Common/src/feup/sdis/protocol/listeners/GetChunkListener.java
@@ -7,9 +7,14 @@ import java.util.Observable;
 /**
  * Created by joaos on 14/05/2016.
  */
-public class GetChunkListener extends BackupProtocol {
+public class GetChunkListener extends BackupProtocol implements Runnable {
     @Override
     public void update(Observable o, Object arg) {
+
+    }
+
+    @Override
+    public void run() {
 
     }
 }
