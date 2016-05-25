174b0e85f02f2113132cf061b8c7221580ef8b24
sceptross <up201304205@fe.up.pt>
2016-05-25 15:00:15 +0100

    WhoAmI protocol enhanced

--- Common/src/feup/sdis/protocol/messages/WhoAmIMessage.java
+++ Common/src/feup/sdis/protocol/messages/WhoAmIMessage.java
@@ -2,13 +2,27 @@ package feup.sdis.protocol.messages;
 
 import feup.sdis.protocol.Protocol;
 
+import java.util.StringJoiner;
 import java.util.UUID;
 
 /**
  * Created by Afonso on 25/05/2016.
  */
 public class WhoAmIMessage extends ProtocolMessage {
+    UUID senderId;
+    
     public WhoAmIMessage(UUID senderId) {
-        super(Type.WHOAMI, Protocol.VERSION, senderId, null);
+        super(Type.WHOAMI, Protocol.VERSION);
+        this.senderId = senderId;
+    }
+
+    @Override
+    public String getHeader(){
+        StringJoiner sj = new StringJoiner(" ", "", CRLF);
+        sj.add(getMessageType().toString())
+                .add("" + getVersion())
+                .add("" + senderId);
+
+        return sj.toString();
     }
 }
