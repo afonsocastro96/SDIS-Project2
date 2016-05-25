4edd2aca291a27593994d96261dc81f1eaae9a41
sceptross <up201304205@fe.up.pt>
2016-05-25 12:41:38 +0100

    Ok Protocol Enhanced

--- Common/src/feup/sdis/protocol/messages/OkMessage.java
+++ Common/src/feup/sdis/protocol/messages/OkMessage.java
@@ -2,12 +2,27 @@ package feup.sdis.protocol.messages;
 
 import feup.sdis.protocol.Protocol;
 
+import java.util.StringJoiner;
+
 /**
  * Created by Afonso on 25/05/2016.
  */
 public class OkMessage extends ProtocolMessage{
 
-    public OkMessage() {
+    final String message;
+
+    public OkMessage(final String message) {
         super(Type.OK, Protocol.VERSION);
+        this.message = message;
+    }
+
+    @Override
+    public String getHeader(){
+        StringJoiner sj = new StringJoiner(" ", "", CRLF);
+        sj.add(getMessageType().toString())
+                .add("" + getVersion())
+                .add("" + message);
+
+        return sj.toString();
     }
 }
--- Common/src/feup/sdis/protocol/messages/parsers/OkParser.java
+++ Common/src/feup/sdis/protocol/messages/parsers/OkParser.java
@@ -4,6 +4,8 @@ import feup.sdis.protocol.exceptions.MalformedMessageException;
 import feup.sdis.protocol.messages.OkMessage;
 import feup.sdis.protocol.messages.ProtocolMessage;
 
+import java.util.StringJoiner;
+
 /**
  * Created by Afonso on 25/05/2016.
  */
@@ -11,12 +13,17 @@ public class OkParser extends ProtocolParser{
     @Override
     public ProtocolMessage parse(byte[] message) throws MalformedMessageException {
         splitMessage(message);
-        if (header.size() != 1)
-            throw new MalformedMessageException("Wrong number of arguments for the OK message: 1 argument must be present");
+        if (header.size() < 3)
+            throw new MalformedMessageException("Wrong number of arguments for the OK message: must have at least 3 arguments");
 
         if (!header.get(0).equalsIgnoreCase(ProtocolMessage.Type.OK.toString()))
             throw new MalformedMessageException("Wrong protocol");
 
-        return new OkMessage();
+        StringJoiner sj = new StringJoiner(" ");
+        for(int i = 2; i < header.size(); i++){
+            sj.add(header.get(i));
+        }
+
+        return new OkMessage(sj.toString());
     }
 }
