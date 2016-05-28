7a207a6221d6bfc1f9656768424f12dfdffcdbcb
João Silva <joaosilva@revtut.net>
2016-05-28 19:50:33 +0100

    Added restore to CLI.

--- Peer/src/feup/sdis/PeerCLI.java
+++ Peer/src/feup/sdis/PeerCLI.java
@@ -12,12 +12,13 @@ import java.util.Scanner;
 public class PeerCLI {
     private boolean exit = false;
 
-    public void run(){
+    public void run() {
         Scanner scanner = new Scanner(System.in);
-        while(true){
+        while (true) {
+            System.out.print("> ");
             String input = scanner.nextLine();
             handleInput(input);
-            if(exit)
+            if (exit)
                 return;
         }
     }
@@ -25,44 +26,54 @@ public class PeerCLI {
     private void handleInput(String input) {
         String[] args = input.split(" ");
         String command = args[0].toLowerCase();
-        switch(command){
+        switch (command) {
             case "backup":
             case "bck":
             case "b":
-                if(args.length != 3)
+                if (args.length != 3) {
                     System.out.println("USAGE: backup <file> <repDegree>");
-                else
-                    if(!BackupCommand.execute(new File(args[1]), Integer.parseInt(args[2])))
+                } else {
+                    if (!BackupCommand.execute(new File(args[1]), Integer.parseInt(args[2])))
                         System.out.println("Could not backup the file...");
+                    else
+                        System.out.println("File was backed up successfully!");
+                }
                 break;
             case "delete":
             case "dlt":
             case "d":
-                if(args.length != 2)
+                if (args.length != 2)
                     System.out.println("USAGE: remove <file>");
-                else {}
+                else {
+                }
                 break;
             case "restore":
             case "rst":
             case "r":
-                if(args.length != 2)
+                if (args.length != 2) {
                     System.out.println("USAGE: restore <file>");
-                else
-                    RestoreCommand.execute(new File(args[1]));
+                } else {
+                    if(!RestoreCommand.execute(new File(args[1])))
+                        System.out.println("Could not restore the file...");
+                    else
+                        System.out.println("File was restored successfully!");
+                }
                 break;
             case "start":
             case "str":
             case "s":
-                if(args.length != 1)
+                if (args.length != 1)
                     System.out.println("USAGE: start");
-                else {}
+                else {
+                }
                 break;
             case "verification":
             case "vrf":
             case "v":
-                if(args.length != 1)
+                if (args.length != 1)
                     System.out.println("USAGE: verification");
-                else {}
+                else {
+                }
                 break;
             case "exit":
             case "ext":
@@ -76,7 +87,7 @@ public class PeerCLI {
         }
     }
 
-    private void backup(){
+    private void backup() {
 
     }
 }
