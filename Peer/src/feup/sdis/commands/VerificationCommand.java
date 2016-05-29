174afaea2105184333785b2e3111b664aef6af9d
João Silva <joaosilva@revtut.net>
2016-05-29 17:26:29 +0100

    Added checksum validation when verifying the system.

--- Peer/src/feup/sdis/commands/VerificationCommand.java
+++ Peer/src/feup/sdis/commands/VerificationCommand.java
@@ -1,9 +1,17 @@
 package feup.sdis.commands;
 
+import feup.sdis.Node;
 import feup.sdis.Peer;
+import feup.sdis.logger.Level;
+import feup.sdis.protocol.Protocol;
 import feup.sdis.protocol.initiators.HasChunkInitiator;
+import feup.sdis.utils.Security;
 
+import java.io.DataInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.UUID;
 
 /**
@@ -38,6 +46,26 @@ public class VerificationCommand implements Command {
                 if (pos > 0)
                     chunkFilename = chunkFilename.substring(0, pos);
                 final int chunkNo = Integer.parseInt(chunkFilename);
+
+                // Validate the checksum
+                final byte[] body;
+                final byte[] buffer = new byte[Protocol.CHUNK_SIZE * 2];
+                final int size;
+                final long checksum;
+                try {
+                    final DataInputStream dataInputStream = new DataInputStream(new FileInputStream(chunk));
+                    checksum = dataInputStream.readLong();
+                    size = dataInputStream.read(buffer);
+                    body = Arrays.copyOf(buffer, size);
+                } catch (IOException e) {
+                    Node.getLogger().log(Level.FATAL, "Could not read the chunk number " + chunkNo + " of the file " + fileId + ". " + e.getMessage());
+                    continue;
+                }
+                final long currentChecksum = Security.checksum(body);
+                if(currentChecksum != checksum) {
+                    chunk.delete();
+                    continue;
+                }
 
                 // Request the server to check if chunk is supposed to exist
                 final HasChunkInitiator hasChunkInitiator = new HasChunkInitiator(Peer.getInstance().getMonitor(), fileId, chunkNo);
