674175ef68bd0a002959156aa863ba1dd188f0d6
João Silva <joaosilva@revtut.net>
2016-05-29 04:01:28 +0100

    Fixed path of data folder.

--- Peer/src/feup/sdis/protocol/listeners/GetChunkListener.java
+++ Peer/src/feup/sdis/protocol/listeners/GetChunkListener.java
@@ -67,12 +67,12 @@ public class GetChunkListener extends ProtocolListener {
             return;
 
         // Create file directory
-        final File fileDir = new File(fileId.toString());
+        final File fileDir = new File("data" + File.separator + fileId.toString());
         if(!fileDir.exists())
             return;
 
         // Create chunk file
-        final File chunkFile = new File("data" + File.separator + fileId.toString() + File.separator + chunkNo + ".bin");
+        final File chunkFile = new File(fileDir.getAbsolutePath() + File.separator + chunkNo + ".bin");
         if(!chunkFile.exists())
             return;
 
